baf80d3a0f46290d4cd3742bf09311c650aa84db
Alex Young <alex.young@hive.hr>
2019-12-15 15:39:54 +0000

    Add path helper

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,69 +1,84 @@
 import express, { Request } from "express";
-import { chain, TaskEither, right, bimap, map } from "fp-ts/lib/TaskEither";
+import {
+    chain,
+    TaskEither,
+    right,
+    bimap,
+    map,
+} from "fp-ts/lib/TaskEither";
+import { ProcessedPath, path } from "./path";
 
 type HttpError = { _type: "err" };
 type HttpResult = { _type: "res" };
-type Handler<T extends {}> = (ctx: T) => TaskEither<HttpError, HttpResult>;
+type Handler<T extends {}, P extends string> = (
+    ctx: T & { params: Record<P, string> }
+) => TaskEither<HttpError, HttpResult>;
 type Middleware<T extends {}, U extends {}> = (
-  ctx: BaseCtx
+    ctx: BaseCtx
 ) => (prev: T) => TaskEither<HttpError, U>;
 
 type BaseCtx = {
-  request: Request;
+    request: Request;
 };
 
-const combineTEither = <T extends object>(t1: TaskEither<HttpError, T>) => <
-  U extends {}
->(
-  t2: U
-): TaskEither<HttpError, T & U> => {
-  return map<T, T & U>(x => ({ ...x, ...t2 }))(t1) as any;
+const combineTEither = <T extends object>(
+    t1: TaskEither<HttpError, T>
+) => <U extends {}>(t2: U): TaskEither<HttpError, T & U> => {
+    return map<T, T & U>(x => ({ ...x, ...t2 }))(t1) as any;
 };
 
-const handler = <T extends {}, U extends T>(
-  ...middleware: Array<Middleware<T, U>>
-) => (impl: Handler<U>) => (
-  ctx: BaseCtx
-): TaskEither<HttpError, HttpResult> => {
-  const done = middleware.reduce((out, m) => {
-    return chain(combineTEither(out))(chain(m(ctx))(out));
-  }, right({}) as TaskEither<HttpError, U>);
-
-  return chain(impl)(done);
+const runMiddleware = <T extends {}, U extends T>(
+    middleware: Array<Middleware<T, U>>
+) => (ctx: BaseCtx): TaskEither<HttpError, U> => {
+    return chain(
+        combineTEither(right({ ...ctx, params: ctx.request.params }))
+    )(
+        middleware.reduce(
+            (out, m) =>
+                chain(combineTEither(out))(chain(m(ctx))(out)),
+            right({}) as TaskEither<HttpError, U>
+        )
+    );
 };
 
 function createHandler(
-  router: ReturnType<typeof express>
-): (
-  path: string
+    router: ReturnType<typeof express>
+): <P extends string>(
+    path: ProcessedPath<P>
 ) => <T extends {}, U extends {}, V extends {}>(
-  middleware: [Middleware<T, U>, Middleware<U, V>]
-) => (handle: Handler<U & V>) => void;
+    middleware: [Middleware<T, U>, Middleware<U, V>]
+) => (handle: Handler<U & V, P>) => void;
 function createHandler(
-  router: ReturnType<typeof express>
-): (
-  path: string
+    router: ReturnType<typeof express>
+): <P extends string>(
+    path: ProcessedPath<P>
 ) => <T extends BaseCtx, U extends T>(
-  middleware: [Middleware<T, U>]
-) => (handle: Handler<U>) => void;
+    middleware: [Middleware<T, U>]
+) => (handle: Handler<U, P>) => void;
 function createHandler(router: ReturnType<typeof express>) {
-  return (path: string) => (middleware: any) => {
-    return (handle: Handler<any>) => {
-      router.get(path, (request, res) => {
-        bimap<HttpError, void, HttpResult, void>(
-          err => res.status(500).send(err),
-          r => res.status(200).send(r)
-        )(handler<any, any>(...middleware)(handle)({ request }))();
-      });
+    return <P extends string>({ path }: ProcessedPath<P>) => (
+        middleware: any
+    ) => (handle: Handler<any, any>) => {
+        router.get(path, (request, res) => {
+            bimap<HttpError, void, HttpResult, void>(
+                err => res.status(500).send(err),
+                r => res.status(200).send(r)
+            )(
+                chain(handle)(
+                    runMiddleware(middleware)({
+                        request,
+                    })
+                )
+            )();
+        });
     };
-  };
 }
 
 const createRoute = (router: ReturnType<typeof express>) => {
-  return {
-    listen: router.listen.bind(router),
-    get: createHandler(router),
-  };
+    return {
+        listen: router.listen.bind(router),
+        get: createHandler(router),
+    };
 };
 
 export const createServer = () => createRoute(express());
@@ -71,15 +86,25 @@ export const createServer = () => createRoute(express());
 const app = createServer();
 
 const m1: Middleware<{}, { hello: string }> = () => () => {
-  return right({ hello: "alex" });
+    return right({ hello: "alex" });
 };
 
-const m2: Middleware<{}, { goodbye: string }> = () => () => {
-  return right({ goodbye: "alex" });
+const m2: Middleware<
+    { hello: string },
+    { goodbye: string }
+> = () => () => {
+    return right({ goodbye: "alex" });
 };
 
-app.get("/")([m1, m2])(({ hello, goodbye }) => {
-  return right({ _type: "res", hello, goodbye });
-});
+app.get(path`/${"name"}`)([m1, m2])(
+    ({ hello, goodbye, params: { name } }) => {
+        return right({
+            _type: "res",
+            hello,
+            goodbye,
+            name,
+        });
+    }
+);
 
 app.listen(3500, console.log);
