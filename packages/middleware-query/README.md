3e0ee02af8e37f21b7ccab707d82137a1e622675
Alex Young <alex.young@hive.hr>
2019-12-15 22:37:12 +0000

    Create query middleware

---
+++ packages/middleware-query/README.md
@@ -0,0 +1,11 @@
+# `@tservit/middleware-query`
+
+> TODO: description
+
+## Usage
+
+```
+const middlewareQuery = require('@tservit/middleware-query');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/middleware-query/src/index.ts
@@ -0,0 +1,20 @@
+import { Middleware, HttpError } from "@tservit/core";
+import { fromEither } from "fp-ts/lib/TaskEither";
+import * as t from "io-ts";
+import { PathReporter } from "io-ts/lib/PathReporter";
+import { bimap, left } from "fp-ts/lib/Either";
+
+export const query = <T extends any>(
+    shape: t.Type<T>
+): Middleware<{}, { query: T }> => ({ request }) => () =>
+    fromEither(
+        bimap<t.Errors, HttpError, T, { query: T }>(
+            errors => ({
+                code: 400,
+                msg: `Invalid query: ${PathReporter.report(left(errors)).join(
+                    ", "
+                )}`
+            }),
+            query => ({ query })
+        )(shape.decode(request.query))
+    );
